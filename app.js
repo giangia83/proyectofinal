a1158683c3b7912b0f310dca7dde9059b4d5242a
giangia83 <giangia83@gmail.com>
2024-06-10 22:10:36 -0400

    renderbuild01
    renderbuild1

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const path = require('path')
+const userRouter = require('./controllers/usuarios')
+
+//conexion a la bd
+try {
+    mongoose.connect('')
+    console.log('Lista la conexion BD')
+} catch (error){
+    console.log(error)
+}
+
+//RUTAS DE FRONTEND
+app.use('/',express.static(path.resolve('views','home')))
+app.use('/cuenta',express.static(path.resolve('views','cuenta')))
+app.use('/informacion',express.static(path.resolve('infocuenta')))
+app.use('/iniciarsesion',express.static(path.resolve('iniciar')))
+app.use('/tuspedidos',express.static(path.resolve('pedidos')))
+app.use('/registrarse',express.static(path.resolve('registrar')))
+app.use('/configuracion',express.static(path.resolve('plantila-configuracion')))
+app.use('/servicioalcliente',express.static(path.resolve('views','serviciocliente')))
+
+//SUPER IMPORTANTE
+app.use(express.json())
+
+//RUTAS DE BACKEND
+app.use('/api/users',userRouter)
+
+module.exports = app
