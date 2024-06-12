9a14e3e7b24d668693829ab32db09cb17a0aa5be
giangia83 <giangia83@gmail.com>
2024-06-12 18:38:20 -0400

    usuarios

--- views/registrar/main.js
+++ views/registrar/main.js
@@ -5,15 +5,42 @@ const registroForm = document.querySelector('#registro-form');
 registroForm.addEventListener('submit', async (event) => {
     event.preventDefault(); // Previene el comportamiento predeterminado de enviar el formulario
 
-    // Aquí puedes realizar cualquier validación adicional del formulario si es necesario
+    // Obtiene los valores ingresados por el usuario en el formulario
+    const nombre = document.querySelector('#inputNombre').value;
+    const correo = document.querySelector('#inputEmail').value;
+    const contraseña = document.querySelector('#inputPassword').value;
+    const direccion = document.querySelector('#inputDireccion').value;
+    const ciudad = document.querySelector('#inputCiudad').value;
 
     try {
-        // Realiza cualquier operación de registro, como enviar datos al servidor, guardar en la base de datos, etc.
+        // Realiza la operación de registro en la base de datos
+        const respuesta = await fetch('/usuarios', { // Reemplaza '/usuarios/registro' con la ruta correspondiente a tu API
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nombre: nombre,
+                correo: correo,
+                contraseña: contraseña,
+                direccion: direccion,
+                ciudad: ciudad
+            })
+        });
 
-        // Después de que el usuario se haya registrado exitosamente, redirige a la página de inicio de sesión
-        window.location.href = '/iniciarsesion';
+        // Verifica si el registro fue exitoso
+        if (respuesta.ok) {
+            // Redirige a la página de inicio de sesión
+            window.location.href = '/iniciarsesion';
+        } else {
+            // Maneja cualquier error de respuesta del servidor
+            console.error('Error al registrar usuario:', respuesta.statusText);
+            // Muestra un mensaje de error al usuario
+            alert('Error al registrar usuario. Por favor, inténtalo de nuevo más tarde.');
+        }
     } catch (error) {
         console.error('Error al procesar el formulario de registro:', error);
         // Maneja el error de acuerdo a tus necesidades, por ejemplo, mostrando un mensaje de error al usuario
+        alert('Error al procesar el formulario de registro. Por favor, inténtalo de nuevo más tarde.');
     }
 });
