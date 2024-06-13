ee903144498316bc01359b02027bba841676b6e5
giangia83 <giangia83@gmail.com>
2024-06-12 20:21:52 -0400

    test20

--- views/registrar/main.js
+++ views/registrar/main.js
@@ -1,10 +1,8 @@
 const registroForm = document.querySelector('#registro-form');
 
-// Escucha el evento de envío del formulario
 registroForm.addEventListener('submit', async (event) => {
-    event.preventDefault(); // Previene el comportamiento predeterminado de enviar el formulario
+    event.preventDefault();
 
-    // Obtiene los valores ingresados por el usuario en el formulario
     const nombre = document.querySelector('#inputNombre').value;
     const correo = document.querySelector('#inputEmail').value;
     const contraseña = document.querySelector('#inputPassword').value;
@@ -12,8 +10,7 @@ registroForm.addEventListener('submit', async (event) => {
     const ciudad = document.querySelector('#inputCiudad').value;
 
     try {
-        // Realiza la operación de registro en la base de datos
-        const respuesta = await axios.post('/api/users', { 
+        const respuesta = await axios.post('/api/users', {
             nombre: nombre,
             correo: correo,
             contraseña: contraseña,
@@ -21,19 +18,15 @@ registroForm.addEventListener('submit', async (event) => {
             ciudad: ciudad
         });
 
-        // Verifica si el registro fue exitoso
         if (respuesta.status === 201) {
-            // Redirige a la página de inicio de sesión
+            console.log('Usuario creado exitosamente');
             window.location.href = '/iniciarsesion';
         } else {
-            // Maneja cualquier error de respuesta del servidor
             console.error('Error al registrar usuario:', respuesta.statusText);
-            // Muestra un mensaje de error al usuario
             alert('Error al registrar usuario. Por favor, inténtalo de nuevo más tarde.');
         }
     } catch (error) {
         console.error('Error al procesar el formulario de registro:', error);
-        // Maneja el error de acuerdo a tus necesidades, por ejemplo, mostrando un mensaje de error al usuario
         alert('Error al procesar el formulario de registro. Por favor, inténtalo de nuevo más tarde.');
     }
 });
