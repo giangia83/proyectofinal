b721009350b97638b5c03a4d7700a4322ddca9ff
giangia83 <giangia83@gmail.com>
2024-06-26 21:46:54 -0400

    prueba6

--- views/iniciar/main.js
+++ views/iniciar/main.js
@@ -17,6 +17,30 @@ async function cargarUsuarios() {
     }
 }
 
+// Función para iniciar sesión en el servidor
+async function iniciarSesion(correo, contraseña) {
+    try {
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ correo, contraseña })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            return data.usuario; // Devolver el usuario si el inicio de sesión fue exitoso
+        } else {
+            throw new Error(data.error || 'Error al iniciar sesión');
+        }
+    } catch (error) {
+        console.error('Error al iniciar sesión:', error);
+        throw error;
+    }
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     // Cargar los usuarios al cargar la página (opcional)
     let usuarios;
@@ -41,32 +65,17 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         try {
             // Buscar el usuario por correo electrónico en la lista cargada
-            const usuario = usuarios.find(user => user.correo === correo);
+            const usuario = await iniciarSesion(correo, contraseña);
 
-            if (!usuario) {
-                throw new Error('Usuario no encontrado');
-            }
-
-            // Realizar la solicitud POST para iniciar sesión utilizando fetch
-            const response = await fetch('/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ correo, contraseña })
-            });
-
-            const data = await response.json();
-
-            if (response.ok) {
-                alert("Inicio de sesión satisfactorio.");
-                // Redirigir al usuario a la página de cuenta después del inicio de sesión exitoso
+            if (usuario) {
+                // El inicio de sesión fue exitoso, redirigir al usuario a la página de cuenta
                 window.location.href = '/cuenta';
             } else {
-                // Mostrar mensaje de error si las credenciales son inválidas
-                mostrarMensaje(data.error || 'Error al iniciar sesión');
+                // Mostrar un mensaje de error si el inicio de sesión falla
+                mostrarMensaje('Credenciales inválidas. Por favor, inténtalo de nuevo.');
             }
         } catch (error) {
+            // Mostrar un mensaje de error si hay algún problema durante el proceso de inicio de sesión
             console.error('Error al iniciar sesión:', error);
             mostrarMensaje('Error al iniciar sesión. Por favor, inténtalo de nuevo más tarde.');
         }
