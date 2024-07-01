3e17f79d79a1f7ad0fc87dc5d5de87e09f0393be
giangia83 <giangia83@gmail.com>
2024-07-01 10:09:21 -0400

    avila.1.4

---
+++ middleware/upload.js
@@ -0,0 +1,18 @@
+const multer = require('multer');
+const path = require('path');
+
+// Configuración de almacenamiento para Multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/uploads'); // Directorio donde se almacenarán los archivos
+    },
+    filename: function (req, file, cb) {
+        // Generar un nombre único para el archivo
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+// Configurar la instancia de Multer
+const upload = multer({ storage: storage });
+
+module.exports = upload;
