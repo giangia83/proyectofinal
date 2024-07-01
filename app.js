806ded0164b9e3b4724437bc7b4cc8170d082c8e
giangia83 <giangia83@gmail.com>
2024-07-01 12:41:28 -0400

    chile8.0

--- app.js
+++ app.js
@@ -23,31 +23,6 @@ const mongoURI = process.env.MONGODB_URI;
 // Configuración de Multer
 
 
-// Ruta para subir un producto con imagen
-router.post('api/subir-producto', upload.single('imagen'), async (req, res) => {
-    // Verificar si se subió correctamente el archivo
-    if (!req.file) {
-        return res.status(400).json({ error: 'No se ha seleccionado ningún archivo para subir.' });
-    }
-
-    // Crear un nuevo producto con la información recibida
-    const nuevoProducto = new Producto({
-        nombre: req.body.nombre,
-        precio: req.body.precio,
-        costo: req.body.costo,
-        categoria: req.body.categoria,
-        imagen: req.file.path.replace('public', '') // Guarda la ruta de la imagen (URL relativa)
-    });
-
-    try {
-        // Guardar el nuevo producto en la base de datos
-        const productoGuardado = await nuevoProducto.save();
-        res.status(201).json({ mensaje: 'Producto subido correctamente', producto: productoGuardado });
-    } catch (error) {
-        console.error('Error al guardar el producto:', error);
-        res.status(500).json({ error: 'Error interno al guardar el producto' });
-    }
-});
 
 // Configuración de Handlebars como motor de plantillas
 
@@ -101,6 +76,40 @@ app.use('/verproductos', express.static(path.resolve(__dirname, 'views', 'produc
 // Rutas de API
 app.use('/api/users', userRouter);
 app.use('/api/productos', productoRouter);
+
+
+
+
+
+
+// Ruta para subir un producto con imagen
+router.post('api/subir-producto', upload.single('imagen'), async (req, res) => {
+    // Verificar si se subió correctamente el archivo
+    if (!req.file) {
+        return res.status(400).json({ error: 'No se ha seleccionado ningún archivo para subir.' });
+    }
+
+    // Crear un nuevo producto con la información recibida
+    const nuevoProducto = new Producto({
+        nombre: req.body.nombre,
+        precio: req.body.precio,
+        costo: req.body.costo,
+        categoria: req.body.categoria,
+        imagen: req.file.path.replace('public', '') // Guarda la ruta de la imagen (URL relativa)
+    });
+
+    try {
+        // Guardar el nuevo producto en la base de datos
+        const productoGuardado = await nuevoProducto.save();
+        res.status(201).json({ mensaje: 'Producto subido correctamente', producto: productoGuardado });
+    } catch (error) {
+        console.error('Error al guardar el producto:', error);
+        res.status(500).json({ error: 'Error interno al guardar el producto' });
+    }
+});
+
+
+
 
 // Rutas de autenticación y sesión
 app.post('/api/login', async (req, res) => {
