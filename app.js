2acee8a3de6ca6f1c1ee8bbc49732519534059c1
giangia83 <giangia83@gmail.com>
2024-07-02 16:20:44 -0400

    Staclean Avila.1.7

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const storage = multer.diskStorage({
   
 
 const upload = multer({ storage: storage }).single('image');
-app.use('/upload', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.post('/upload', (req, res) => {
     upload(req, res, (err) => {
