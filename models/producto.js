83c06e22e27131ae604a15021718bbc2732413a1
giangia83 <giangia83@gmail.com>
2024-07-02 15:14:42 -0400

    ironmaiden

--- models/producto.js
+++ models/producto.js
@@ -1,39 +1,20 @@
 const mongoose = require('mongoose');
 
+
 const productoSchema = new mongoose.Schema({
-
-    nombre:{
-
-        type:String,
-        required:true
-
-
-    },
-
-    precio: {
-        type: String,
-        required: true
-    },
-    costo: {
-        type: String,
-        required: true
-      
-    },
-    categoria: {
-        type: String,
-        required: true
-    },
-
-    image:{
-
-        type:String,
-        required: true
-
+    nombre: { type: String, required: true },
+    costo: { type: Number, required: true },
+    precio: { type: Number, required: true },
+    categoria: { type: String, required: true },
+    image: {
+        data: Buffer, // Datos binarios del archivo
+        contentType: String // Tipo de contenido del archivo (ej. 'image/png')
     }
+});
 
 
  
-})
+
 
 
 const Producto = mongoose.model('Producto', productoSchema);
