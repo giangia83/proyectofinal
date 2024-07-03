5d5b4eb44d5ef6594f88ddeec2160575f66436d0
giangia83 <giangia83@gmail.com>
2024-07-03 14:17:21 -0400

    Starclean RENDER

--- app.js
+++ app.js
@@ -108,7 +108,9 @@ app.use(express.static(path.join(__dirname,'./public')))
 app.set('view engine', 'ejs');
 app.use('/views', express.static(path.join(__dirname, 'views')));
 
-
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 
 app.use( 'public', express.static(path.join(__dirname, 'public')));
