2ae0992bcb46077194a9c01cf35db5ea6db61f20
giangia83 <giangia83@gmail.com>
2024-07-04 16:06:06 -0400

    Starclean Z4.9

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ app.use('/administrar', express.static(path.resolve(__dirname, 'views', 'admin')
 app.use('/cotizaciones', express.static(path.resolve(__dirname, 'views', 'cotizaciones')));
 app.use('/verproductos', express.static(path.resolve(__dirname, 'views', 'productos')));
 
-
+app.use("/main",express.static(__dirname + '/main'));
 
 
 
