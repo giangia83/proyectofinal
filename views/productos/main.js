1dda63daf6f6aaeca9ee0fabfe896548fde53b70
giangia83 <giangia83@gmail.com>
2024-07-04 14:44:49 -0400

    Starclean Z4.2

--- views/productos/main.js
+++ views/productos/main.js
@@ -1,54 +1,23 @@
 // scripts.js
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Obtener todas las tarjetas de producto
     const cards = document.querySelectorAll('.myCard');
 
-    // Función para marcar como seleccionado al hacer click
-    function selectCard(card) {
-        // Cambiar tamaño y opacidad del div
-        card.style.transform = 'scale(0.95)';
-        card.style.opacity = '0.7';
-
-        // Reemplazar imagen por icono de check
-        const img = card.querySelector('.img-fluid');
-        img.style.filter = 'blur(3px)';
-        
-        // Mostrar el icono de check
-        const checkIcon = card.querySelector('.check-icon');
-        checkIcon.style.display = 'inline'; // Cambiado a 'inline' para mostrar el icono
-
-        // Agregar la clase 'selected' para estilos adicionales si es necesario
-        card.classList.add('selected');
-    }
-
-    // Añadir evento de click a cada tarjeta de producto
     cards.forEach(card => {
         card.addEventListener('click', () => {
-            selectCard(card);
+            // Remover la clase 'selected' de todas las tarjetas
+            cards.forEach(c => c.classList.remove('selected'));
+            // Agregar la clase 'selected' a la tarjeta clickeada
+            card.classList.add('selected');
+
+            // Obtener elementos dentro de la tarjeta
+            const img = card.querySelector('.product-image');
+            const checkIcon = card.querySelector('.check-icon');
+
+            // Aplicar transiciones
+            img.style.opacity = '0';
+            img.style.filter = 'blur(3px)';
+            checkIcon.style.transform = 'translate(-50%, -50%) scale(1)';
         });
     });
-
-    // Script para agregar al carrito y mostrar alerta
-    const addToCartButtons = document.querySelectorAll('.addToCart');
-
-    addToCartButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const productName = button.getAttribute('data-nombre');
-            showAlert(productName);
-        });
-    });
-
-    function showAlert(productName) {
-        const alert = document.createElement('div');
-        alert.className = 'alert alert-success';
-        alert.textContent = `${productName} ha sido agregado al carrito`;
-
-        const container = document.querySelector('.container'); // Selector adecuado para el contenedor de la página
-        container.appendChild(alert);
-
-        setTimeout(() => {
-            alert.remove();
-        }, 5000); // Desaparece la alerta después de 5 segundos
-    }
 });
