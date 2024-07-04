c97c86903f86dea7d1e6d5f354c06b2e36f5c788
giangia83 <giangia83@gmail.com>
2024-07-04 10:34:46 -0400

    Starclean CLOUD 13X

--- app.js
+++ app.js
@@ -142,11 +142,6 @@ app.get('/iniciarsesion', (req, res) => {
     });
 });
 
-app.get('/gestion', (req, res) => {
-    res.render('gestionar/index', {
-        usuario: res.locals.usuario || { nombre: '' }
-    });
-});
 
 
 
