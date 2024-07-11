4f3ee81ba21890ab7d4b2147c98d3cd67f8c58ee
giangia83 <giangia83@gmail.com>
2024-07-11 13:59:22 -0400

    Starclean C1

--- views/home/main.js
+++ views/home/main.js
@@ -1,1 +1,9 @@
+ // JavaScript para manejar el menú hamburguesa en pantallas móviles
+ const menuHamburguesa = document.getElementById('menuHamburguesa');
+ const menuDesplegable = document.getElementById('menuDesplegable');
 
+ if (menuHamburguesa && menuDesplegable) {
+     menuHamburguesa.addEventListener('click', () => {
+         menuDesplegable.classList.toggle('activo');
+     });
+ }
