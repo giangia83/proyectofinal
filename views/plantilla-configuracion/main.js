ae428cfb59869d5a35ec74c2be55e790d5110d2e
giangia83 <giangia83@gmail.com>
2024-07-18 09:09:44 -0400

    Star1

--- views/plantilla-configuracion/main.js
+++ views/plantilla-configuracion/main.js
@@ -1,32 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-      
-    // Manejar el envío del formulario
-    document.getElementById('configuracionForm').addEventListener('submit', function (event) {
-      // Prevenir el envío por defecto del formulario
-      event.preventDefault();
-      
-      // Realizar una confirmación opcional aquí si es necesario
-
-      // Enviar el formulario usando fetch
-      fetch(this.action, {
+document.getElementById('configuracionForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Evita el envío predeterminado del formulario
+    
+    // Obtén los datos del formulario
+    const formData = new FormData(this);
+  
+    try {
+      const response = await fetch(this.action, {
         method: this.method,
-        body: new FormData(this)
-      })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Error en la respuesta del servidor');
-        }
-        return response.json();
-      })
-      .then(data => {
-        // Manejar la respuesta del servidor aquí (por ejemplo, mostrar un mensaje de éxito)
-        console.log('Respuesta del servidor:', data);
-        alert('¡Actualización exitosa!');
-      })
-      .catch(error => {
-        // Manejar errores de red o del servidor
-        console.error('Error en la solicitud:', error);
-        alert('Ocurrió un error al actualizar la información.');
+        body: formData
       });
-    });
+  
+      if (!response.ok) {
+        throw new Error('Error en la solicitud: ' + response.statusText);
+      }
+  
+      // Manejar la respuesta como sea necesario
+      console.log('Solicitud exitosa');
+      // Puedes redirigir o mostrar un mensaje de éxito aquí
+  
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
+      // Manejar errores y mostrar mensajes al usuario
+    }
   });
+  
