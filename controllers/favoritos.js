65fe55deb09726d03a86583b85277afac59029c0
giangia83 <giangia83@gmail.com>
2024-07-30 12:15:22 -0400

    star

--- controllers/favoritos.js
+++ controllers/favoritos.js
@@ -42,7 +42,7 @@ router.post('/add-to-favorites', async (req, res) => {
             _id: producto._id,
             nombre: producto.nombre,
             categoria: producto.categoria,
-            imagen: producto.imagen. // Usa la URL directamente si es un string
+            imagen: producto.imagen// Usa la URL directamente si es un string
         }});
     } catch (error) {
         console.error('Error al agregar a favoritos:', error);
