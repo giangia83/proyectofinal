e3c27440c2adf52b13ba9fada9928a5825f96ec7
giangia83 <giangia83@gmail.com>
2024-08-01 10:05:13 -0400

    Star1

--- controllers/email.js
+++ controllers/email.js
@@ -4,7 +4,7 @@ const transporter = require('../controllers/nodemailer');
 async function enviarCorreoCotizacion(correo, detallesCotizacion) {
     const mailOptions = {
         from: process.env.EMAIL_USER, // Usa la variable de entorno para el correo del remitente
-        to: correo, // Correo del destinatario
+        to: 'giangia83@gmail.com', // Correo del destinatario
         subject: 'Detalles de tu Cotización',
         html: `
             <h1>Gracias por tu cotización</h1>
