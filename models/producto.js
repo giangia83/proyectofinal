9be82acd958df87513c0316a9016dfd6df70c9b8
giangia83 <giangia83@gmail.com>
2024-09-10 10:18:00 -0400

    star2

--- models/producto.js
+++ models/producto.js
@@ -7,15 +7,10 @@ const productoSchema = new mongoose.Schema({
     precio: { type: Number, required: true },
     categoria: { type: String, required: true },
     imagen: {
-        data: { type: String, required: true }, // Cambiado a String para la URL de la imagen
-        contentType: { type: String, required: true } // Tipo de contenido del archivo (ej. 'image/png')
+        data: { type: String, required: true }, 
+        contentType: { type: String, required: true } 
     }
 });
-
-
- 
-/*  */
-
 
 const Producto = mongoose.model('Producto', productoSchema);
 
